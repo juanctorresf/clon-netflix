54efd8049eea9a45ee347e1b8feb7d5953141c80
Juan C Torres <jtorres.devv@gmail.com>
2023-03-03 16:43:37 -0500

    Agregando sourcemaps y cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const { src, dest, watch, series } = require( 'gulp' );
 const sass = require( 'gulp-sass' ) ( require('sass') );
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 
 // IMAGENES
 const imagemin = require('gulp-imagemin');
@@ -14,8 +16,10 @@ const avif = require('gulp-avif');
 // Tareas
 function css() {
     return src( 'src/scss/app.scss' )
+        .pipe( sourcemaps.init() )
         .pipe( sass() )
-        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.') )
         .pipe( dest('build/css') )
 }
 
