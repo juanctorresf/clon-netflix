0f9f7581ce8c3df8e8eff4aba7f35680484dc7d3
Juan C Torres <jtorres.devv@gmail.com>
2023-04-22 15:38:36 -0500

    Mejorando la funcionalidad en el cambio de icono en el selector de idiomas

--- build/js/app.js
+++ build/js/app.js
@@ -1,62 +1,61 @@
-// Selector de select de idiomas
-const idiomas1 = document.querySelector('#idiomas1');
-const idiomas2 = document.querySelector('#idiomas2');
-
-// Selectores de botones de preguntas
-const preguntaBoton = document.querySelectorAll('.pregunta__boton');
-const preguntaDescripcion = document.querySelectorAll('.pregunta__descripcion');
-const imgAbrir = document.querySelectorAll('#imgAbrir');
-
 // Event Listener
-window.addEventListener('resize', addIdioma);
-document.addEventListener('DOMContentLoaded', addIdioma );
+document.addEventListener('DOMContentLoaded', iniciarApp );
 
+function iniciarApp() {
+    window.addEventListener('resize', addIdioma);
 
-// Funcion para mostrar descripcion en la seccion de preguntas
-preguntaBoton.forEach( (boton, i) => {
+    // Selector de select de idiomas
+    const idiomas = document.querySelectorAll('#idioma');
     
-    // Agrega en cada boton el evento de click
-    boton.addEventListener('click', () => {
-        // Verifica que si no se muestra la descripcion al hacer click
-        if(preguntaDescripcion[i].classList.contains('hide')) {
-            // Cierra todas las descripiones
-            preguntaDescripcion.forEach( preg => {
-                preg.classList.add('hide');
+    // Selectores de botones de preguntas
+    const preguntaBoton = document.querySelectorAll('.pregunta__boton');
+    const preguntaDescripcion = document.querySelectorAll('.pregunta__descripcion');
+    const imgAbrir = document.querySelectorAll('#imgAbrir');
+
+    // Funcion para mostrar descripcion en la seccion de preguntas
+    preguntaBoton.forEach( (boton, i) => {
+        
+        // Agrega en cada boton el evento de click
+        boton.addEventListener('click', () => {
+            // Verifica que si no se muestra la descripcion al hacer click
+            if(preguntaDescripcion[i].classList.contains('hide')) {
+                // Cierra todas las descripiones
+                preguntaDescripcion.forEach( preg => {
+                    preg.classList.add('hide');
+                } )
+    
+                // Mostrar descripcion donde se hizo click
+                preguntaDescripcion[i].classList.remove('hide');
+                
+                // Cambiar rotación de todos los botones para parecer a "+"
+                imgAbrir.forEach( img => {
+                    img.classList.remove('open');
+                } )
+    
+                // Cambia rotacion del boton cuando una descripcion de muestra "x"
+                imgAbrir[i].classList.add('open');
+    
+            } else {
+                preguntaDescripcion[i].classList.add('hide');
+                imgAbrir[i].classList.remove('open');
+            }
+        })
+    })    
+    
+    
+    // Function para quitar simbolo del idioma conforma al tamaño de pantalla
+    function addIdioma() {
+        
+        const screenWidth = window.innerWidth;
+    
+        if(screenWidth < 600) {
+            idiomas.forEach( idioma => {
+                idioma.value = '';
             } )
-
-            // Mostrar descripcion donde se hizo click
-            preguntaDescripcion[i].classList.remove('hide');
-            
-            // Cambiar rotación de todos los botones para parecer a "+"
-            imgAbrir.forEach( img => {
-                img.classList.remove('open');
-            } )
-
-            // Cambia rotacion del boton cuando una descripcion de muestra "x"
-            imgAbrir[i].classList.add('open');
-
         } else {
-            preguntaDescripcion[i].classList.add('hide');
-            imgAbrir[i].classList.remove('open');
+            idiomas.forEach( idioma => {
+                idioma.value = 'espanol';
+            } )
         }
-    })
-})    
-
-
-// Funtions para quitar simbolo del idioma conforma al tamaño de pantalla
-function addIdioma() {
-    // width of screen
-    const screenWidth = window.innerWidth;
-
-    leeWidth(screenWidth);
-}
-
-function leeWidth(screenWidth) {
-    if(screenWidth < 600) {
-        idiomas1.value = '';
-        idiomas2.value = '';
-    } else {
-        idiomas1.value = 'espanol';
-        idiomas2.value = 'espanol';
     }
 }
